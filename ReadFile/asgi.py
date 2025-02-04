ea73b16aae968656dc8609dd17445cc32aeeb6a9
ahmadbackend <107940707+ahmadbackend@users.noreply.github.com>
2025-02-04 12:07:02 +0200

    .

---
+++ ReadFile/asgi.py
@@ -0,0 +1,25 @@
+"""
+ASGI config for ReadFile project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ReadFile.settings')
+
+django_asgi_app = get_asgi_application()
+from WebSock.routing import websocket_urlpatterns
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket":URLRouter(websocket_urlpatterns),
+    # Just HTTP for now. (We can add other protocols later.)
+})
